28f28ba70dc6f9acb2f014255c7b0254d68cdf15
Dominick <1hackaweek@gmail.com>
2021-08-14 11:22:22 -0400

    Getting current Fishbowl SO data is now done inside a thread.

--- gui_main.py
+++ gui_main.py
@@ -2,7 +2,7 @@ from cutlistgenerator.appdataclasses.systemproperty import SystemProperty
 import sys, os, traceback, datetime
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMessageBox
-# from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QRunnable, QThreadPool, pyqtSlot, pyqtSignal, QObject
 
 
 import cutlistgenerator
@@ -21,6 +21,35 @@ __version__ = "0.1.0"
 
 logger = FileLogger(__name__)
 
+class Worker(QRunnable):
+    '''
+    Worker thread
+    '''
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        result = self.fn(*self.args, **self.kwargs)
+        self.signals.result.emit(result)
+        self.signals.finished.emit()
+
+class WorkerSignals(QObject):
+    progress = pyqtSignal(int)
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+
+
 class Application(QtWidgets.QMainWindow):
     def __init__(self):
         super(Application, self).__init__()
@@ -30,6 +59,7 @@ class Application(QtWidgets.QMainWindow):
         self.setWindowTitle(f"{PROGRAM_NAME} v{__version__}")
 
         self.settings = cutlistgenerator.program_settings
+        self.threadpool = QThreadPool()
 
 
         # Create settings file if it doesn't exist
@@ -54,29 +84,39 @@ class Application(QtWidgets.QMainWindow):
             self.settings.set_database_setup(True)
             logger.info("[DATABASE] Database setup complete.")
 
-        self.ui.actionGet_Current_SO_Data_From_Fishbowl.triggered.connect(self.get_current_fb_data)
+        self.ui.actionGet_Current_SO_Data_From_Fishbowl.triggered.connect(self.thread_get_current_fb_data)
         auto_update_fishbowl_so_data = SystemProperty.find_by_name(self.cut_list_generator_database, "fishbowl_auto_update_sales_orders")
         # if auto_update_fishbowl_so_data.value:
         #     logger.info("[AUTO UPDATE] Auto updating sales order data from Fishbowl.")
         #     self.get_current_fb_data()
 
-    def get_current_fb_data(self):
+    def thread_get_current_fb_data(self):
+        logger.info("[TREAD] Starting thread to get current sales order data from Fishbowl.")
+        self.ui.statusbar.showMessage("Pulling current sales order data from Fishbowl.")
+        self.ui.actionGet_Current_SO_Data_From_Fishbowl.setEnabled(False)
+        worker = Worker(fn=self.get_current_fb_data, fishbowl_database=self.fishbowl_database, cut_list_database=self.cut_list_generator_database)
+        worker.signals.finished.connect(lambda: self.ui.statusbar.showMessage("Finished pulling sales order data from Fishbowl."))
+        worker.signals.finished.connect(lambda: self.ui.actionGet_Current_SO_Data_From_Fishbowl.setEnabled(True))
+        worker.signals.finished.connect(self.show_fishbowl_update_finished_message_box)
+        self.threadpool.start(worker)
+    
+    @staticmethod
+    def get_current_fb_data(fishbowl_database, cut_list_database):
+        # TODO: Rework this to enable pySignals to be used.
         start_time = datetime.datetime.now()
-        total_rows, rows_inserted = utilities.update_sales_order_data_from_fishbowl(self.fishbowl_database, self.cut_list_database)
+        total_rows, rows_inserted = utilities.update_sales_order_data_from_fishbowl(fishbowl_database, cut_list_database)
         end_time = datetime.datetime.now()
         time_delta = end_time - start_time
         logger.info(f"[EXECUTION TIME]: {time_delta.total_seconds() *1000} ms")
-        
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText(f"{total_rows} rows available to import from Fishbowl.\n{rows_inserted} rows were inserted into the cutlist table.")
 
-        # TODO: Change detailed text to something more meaningful.
-        # msg.setDetailedText(f"{total_rows} rows available to import from Fishbowl.\n{rows_inserted} rows were inserted into the cutlist table.\n\nExecution time: {time_delta.total_seconds() *1000} ms")
-        msg.setWindowTitle("Fishbowl Data")
+    def show_fishbowl_update_finished_message_box(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Fishbowl")
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Fishbowl data update complete.")
+
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec()
-
 
 
 def main():
