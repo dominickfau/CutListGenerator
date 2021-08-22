316de6e63c16882eb6ae5bb42a3aa076640c5b13
dominickfau <1hackaweek@gmail.com>
2021-08-22 11:14:13 -0400

    Created a file to compile all ui files.

---
+++ compile_ui.bat
@@ -0,0 +1,5 @@
+pyuic5 .\cutlistgenerator\ui\cutjobdialog_ui.ui -o .\cutlistgenerator\ui\cutjobdialog_ui.py
+pyuic5 .\cutlistgenerator\ui\cutjobsearchdialog_ui.ui -o .\cutlistgenerator\ui\cutjobsearchdialog_ui.py
+pyuic5 .\cutlistgenerator\ui\mainwindow_ui.ui -o .\cutlistgenerator\ui\mainwindow_ui.py
+pyuic5 .\cutlistgenerator\ui\productdialog_ui.ui -o .\cutlistgenerator\ui\productdialog_ui.py
+pyuic5 .\cutlistgenerator\ui\wirecutterdialog_ui.ui -o .\cutlistgenerator\ui\wirecutterdialog_ui.py
